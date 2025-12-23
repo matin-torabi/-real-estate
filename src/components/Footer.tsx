290bb140e91ad4d0f9a6e94f814e75a35655f4bd
matin <sajjad.torabi95@gmail.com>
2025-12-23 22:39:46 +0330

    change phone

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,32 +11,23 @@ function Footer() {
             <h3 className="text-xl font-bold mb-3">آدرس</h3>
             <p className="text-sm leading-7">
               قرچک، کمربندی شمالی، باهنر بیست و پنجم، قرچک <br />
-              طبقه دوم، واحد ۵
             </p>
           </div>
 
           <div>
             <h3 className="text-xl font-bold mb-3">تماس با ما</h3>
             <p className="text-sm leading-7">
-              تلفن: 021-12345678
+              موبایل:09359506090
               <br />
-              موبایل: 0912-0000000
-              <br />
-              ایمیل: info@example.com
+              موبایل:09126986911
             </p>
           </div>
 
           <div>
             <h3 className="text-xl font-bold mb-3">شبکه های مجازی</h3>
             <div className="flex items-center gap-5 mt-5">
-              <Link href="">
+              <Link href="https://t.me/shapouramlak">
                 <FaTelegram className="text-3xl text-gray-600 hover:text-blue-400 duration-300" />
-              </Link>
-              <Link href="">
-                <FaWhatsapp className="text-3xl text-gray-600 hover:text-green-400 duration-300" />
-              </Link>
-              <Link href="">
-                <FaInstagram className="text-3xl text-gray-600 hover:text-red-500 duration-300" />
               </Link>
             </div>
           </div>
